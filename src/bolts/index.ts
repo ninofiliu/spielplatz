7b735322280762f8f468ed1bf4334b2160391d37
Nino Filiu <nino.filiu@gmail.com>
2021-08-19 00:08:56 +0200

    bolts

---
+++ src/bolts/index.ts
@@ -0,0 +1,84 @@
+document.body.style.margin = '0';
+document.body.style.overflow = 'hidden';
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+const canvas = document.createElement('canvas');
+canvas.width = width;
+canvas.height = height;
+document.body.append(canvas);
+
+const gl = canvas.getContext('webgl');
+
+const createShader = async (type, url) => {
+  const resp = await fetch(url);
+  const source = await resp.text();
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(shader));
+  }
+  return shader;
+};
+
+const createProgram = async () => {
+  const vertexShader = await createShader(gl.VERTEX_SHADER, new URL('./vertex.glsl', import.meta.url));
+  const fragmentShader = await createShader(gl.FRAGMENT_SHADER, new URL('./fragment.glsl', import.meta.url));
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error(gl.getProgramInfoLog(program));
+  }
+  gl.useProgram(program);
+  return program;
+};
+
+(async () => {
+  const program = await createProgram();
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  const positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program);
+
+  // Turn on the attribute
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  const size = 2; // 2 components per iteration
+  const type = gl.FLOAT; // the data is 32bit floats
+  const normalize = false; // don't normalize the data
+  const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, offset,
+  );
+
+  // draw
+  const primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  const count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+})();
