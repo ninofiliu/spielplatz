77781f5de94711d1532ab4d2765645e0f1eaf7f1
Nino Filiu <nino.filiu@gmail.com>
2021-08-20 00:25:25 +0200

    more bolts

--- src/bolts/index.ts
+++ src/bolts/index.ts
@@ -48,9 +48,12 @@ const createProgram = async () => {
   gl.vertexAttribPointer(locations.a_position, 2, gl.FLOAT, false, 0, 0);
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    0, 0,
-    0, 0.5,
-    0.7, 0,
+    -1, -1,
+    -1, 1,
+    1, -1,
+    1, -1,
+    -1, 1,
+    1, 1,
   ]), gl.STATIC_DRAW);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 })();
