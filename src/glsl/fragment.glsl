440d859005a4983d01f0b3f2e0868027df7a7c3b
Nino Filiu <nino.filiu@gmail.com>
2021-09-30 19:16:06 +0200

    fractal worley

--- src/glsl/fragment.glsl
+++ src/glsl/fragment.glsl
@@ -3,8 +3,6 @@ uniform vec2 u_mouse;
 uniform float u_now;
 varying vec4 v_position;
 
-const int nb = 5;
-
 int mod(int a, int n) {
   if (a<0) { return a+n; }
   if (a>=n) { return a-n; }
@@ -12,21 +10,20 @@ int mod(int a, int n) {
 }
 float rand(float seed) {
   return fract(353185.784*sin(seed));
-  // return fract((353.784+u_now*0.0001)*sin(seed));
 }
-float rand(int xi, int yi, int zi) {
+float rand(int nb, int xi, int yi, int zi) {
   return rand(float(xi+nb*yi+nb*nb*zi));
 }
-vec3 get_point(int xi, int yi, int zi) {
+vec3 get_point(int nb, int xi, int yi, int zi) {
   return vec3(
-    (float(xi)+rand(1+xi, yi, zi))/float(nb),
-    (float(yi)+rand(1+xi, yi, 2*zi))/float(nb),
-    (float(zi)+rand(1+xi, yi, 3*zi))/float(nb)
+    (float(xi)+rand(nb, 1+xi, yi, zi))/float(nb),
+    (float(yi)+rand(nb, 1+xi, yi, 2*zi))/float(nb),
+    (float(zi)+rand(nb, 1+xi, yi, 3*zi))/float(nb)
   );
 }
 
-void main () {
-  vec3 current = vec3(v_position.x, v_position.y, mod(u_now*0.0005, 1.0));
+float worley(int nb, float speed) {
+  vec3 current = vec3(v_position.x, v_position.y, mod(u_now*speed, 1.0));
   current.x = mod(current.x+0.5, 1.0);
   current.y = mod(current.y+0.5, 1.0);
   int xi = int(current.x * float(nb));
@@ -40,7 +37,7 @@ void main () {
       int pyi = mod(yi + dy, nb);
       for (int dz = -1; dz < 2; dz++) {
         int pzi = mod(zi + dz, nb);
-        vec3 point = get_point(pxi, pyi, pzi);
+        vec3 point = get_point(nb, pxi, pyi, pzi);
         if (xi==0 && pxi==nb-1) { point.x-=1.0; }
         if (xi==nb-1 && pxi==0) { point.x+=1.0; }
         if (yi==0 && pyi==nb-1) { point.y-=1.0; }
@@ -52,8 +49,13 @@ void main () {
     }
   }
 
-  float light = 6.3 * atan(min_dist);
+  return float(nb) * 0.63 * atan(min_dist);
 
-  gl_FragColor = vec4(light*vec3(0.9,1.0,1.0)+(1.0-light)*vec3(0.3,0.4,0.5), 1.0);
-  // gl_FragColor = vec4(vec3(light), 1.0);
+}
+
+void main () {
+  float wa = worley(3, 0.0005);
+  float wb = worley(6, 0.0004);
+  float light = 0.8 * wa + 0.2 * wb;
+  gl_FragColor = vec4(vec3(light), 1.0);
 }
