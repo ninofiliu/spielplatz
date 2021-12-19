15b44c33469847f6777067aa7ce5e7a078c5999d
Nino Filiu <nino.filiu@gmail.com>
2021-12-19 17:06:18 +0100

    set new on space press

--- src/irsika/index.ts
+++ src/irsika/index.ts
@@ -28,9 +28,8 @@ const crop = (image: HTMLImageElement, width: number, height: number, blur: numb
 
 (async () => {
   const offset = { x: 1.0, y: 1.0 };
-  const force = 0.9;
+  const force = 0.7;
   const blur = 0;
-  let step = 0;
 
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -70,40 +69,54 @@ const crop = (image: HTMLImageElement, width: number, height: number, blur: numb
   addTexture(gl, 2, gl.getUniformLocation(program, 'u_offsets_0'));
   addTexture(gl, 3, gl.getUniformLocation(program, 'u_offsets_1'));
 
+  let step = 0;
+  let srcStep = 0;
   const maxSrcStep = 400;
   let srcImage0 = srcImages[~~(Math.random() * srcImages.length)];
   let srcImage1 = srcImages[~~(Math.random() * srcImages.length)];
+  let dstStep = 0;
   const maxDstStep = 200;
   let dstID0 = dstIDs[~~(Math.random() * dstIDs.length)];
   let dstID1 = dstIDs[~~(Math.random() * dstIDs.length)];
+
+  const setNewSrc = () => {
+    srcImage0 = srcImage1;
+    srcImage1 = srcImages[~~(Math.random() * srcImages.length)];
+    setTextureImage(gl, 0, srcImage0);
+    setTextureImage(gl, 1, srcImage1);
+  };
+
+  const setNewDst = () => {
+    dstID0 = dstID1;
+    dstID1 = dstIDs[~~(Math.random() * dstIDs.length)];
+    setTextureImage(gl, 2, dstID0);
+    setTextureImage(gl, 3, dstID1);
+  };
+
   const loop = () => {
     gl.uniform2f(gl.getUniformLocation(program, 'u_offset'), offset.x, offset.y);
     gl.uniform1f(gl.getUniformLocation(program, 'u_force'), force);
     gl.uniform1f(gl.getUniformLocation(program, 'u_time'), step / 60);
 
-    const srcStep = step % maxSrcStep;
-    if (srcStep === 0) {
-      srcImage0 = srcImage1;
-      srcImage1 = srcImages[~~(Math.random() * srcImages.length)];
-      setTextureImage(gl, 0, srcImage0);
-      setTextureImage(gl, 1, srcImage1);
-    }
+    if (srcStep === 0) setNewSrc();
     const srcMix = (srcStep / maxSrcStep) ** 4;
     gl.uniform1f(gl.getUniformLocation(program, 'u_mix_src'), srcMix);
+    srcStep = (srcStep + 1) % maxSrcStep;
 
-    const dstStep = step % maxDstStep;
-    if (dstStep === 0) {
-      dstID0 = dstID1;
-      dstID1 = dstIDs[~~(Math.random() * dstIDs.length)];
-      setTextureImage(gl, 2, dstID0);
-      setTextureImage(gl, 3, dstID1);
-    }
+    if (dstStep === 0) setNewDst();
     const dstMix = dstStep / maxDstStep;
     gl.uniform1f(gl.getUniformLocation(program, 'u_mix_dst'), dstMix);
+    dstStep = (dstStep + 1) % maxDstStep;
 
     gl.drawArrays(gl.TRIANGLES, 0, 6);
     step++;
     requestAnimationFrame(loop);
   };
   loop();
+
+  document.addEventListener('keypress', (evt) => {
+    if (evt.key !== ' ') return;
+    setNewSrc();
+    setNewDst();
+  });
 })();
