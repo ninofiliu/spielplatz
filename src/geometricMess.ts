8445338543b16a6e8260fd51d54ff4bd99141037
Nino Filiu <nino.filiu@gmail.com>
2022-01-12 22:14:37 +0100

    rectangles

--- src/geometricMess.ts
+++ src/geometricMess.ts
@@ -6,6 +6,7 @@
   canvas.height = height;
   document.body.style.margin = '0';
   document.body.style.overflow = 'hidden';
+  document.body.style.imageRendering = 'pixelated';
   document.body.append(canvas);
 
   const ctx = canvas.getContext('2d');
@@ -21,5 +22,55 @@
       });
     })
     .map((arr) => `#${[...arr].map((n) => n.toString(16).padStart(2, '0')).join('')}`);
-  console.log(palette);
+
+  const createRectStep = () => {
+    const maxNbRects = 5;
+    const maxSize = ~~(0.3 * Math.min(width, height));
+    const maxV = 100;
+
+    const nbRects = 1 + ~~((maxNbRects - 1) * Math.random());
+    const rects = Array(nbRects).fill(null).map(() => {
+      const w = ~~(maxSize * Math.random());
+      const h = ~~(maxSize * Math.random());
+      const x = ~~((width - w) * Math.random());
+      const y = ~~((width - h) * Math.random());
+      const vx = ~~((-0.5 + Math.random()) * maxV);
+      const vy = ~~((-0.5 + Math.random()) * maxV);
+      const fill = palette[~~(palette.length * Math.random())];
+      const stroke = palette[~~(palette.length * Math.random())];
+      return {
+        x,
+        y,
+        w,
+        h,
+        vx,
+        vy,
+        fill,
+        stroke,
+      };
+    });
+
+    return (step) => {
+      const rect = rects[step % rects.length];
+      ctx.fillStyle = rect.fill;
+      ctx.strokeStyle = rect.stroke;
+      for (const dx of [-width, 0]) {
+        for (const dy of [-height, 0]) {
+          ctx.fillRect(rect.x + dx, rect.y + dy, rect.w, rect.h);
+          ctx.strokeRect(rect.x + dx, rect.y + dy, rect.w, rect.h);
+        }
+      }
+      rect.x = (rect.x + rect.vx + width) % width;
+      rect.y = (rect.y + rect.vy + height) % height;
+    };
+  };
+
+  const rectStep = createRectStep();
+  let step = 0;
+  const loop = () => {
+    rectStep(step);
+    step++;
+    requestAnimationFrame(loop);
+  };
+  loop();
 })();
