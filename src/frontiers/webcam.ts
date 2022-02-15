09c97e8ca31de49a3350631b19b2612ecc474c44
Nino Filiu <nino.filiu@gmail.com>
2022-02-15 18:17:33 +0100

    replayable

--- src/frontiers/webcam.ts
+++ src/frontiers/webcam.ts
@@ -1,47 +1,54 @@
-(async () => {
-  const NB_PS = 1000;
-  const SPEED = 16;
+const video = document.createElement('video');
+const inputCanvas = document.createElement('canvas');
+const inputCtx = inputCanvas.getContext('2d');
+const outputCanvas = document.createElement('canvas');
+const outputCtx = outputCanvas.getContext('2d');
+document.body.style.transform = 'scaleX(-1)';
+video.style.position = 'fixed';
+document.body.append(video);
+outputCanvas.style.position = 'fixed';
+outputCanvas.style.opacity = '0.7';
+document.body.append(outputCanvas);
+let animate = () => {};
+
+const play = async (nb: number, speed: number) => {
+  animate = () => {};
+  await new Promise((resolve) => requestAnimationFrame(resolve));
 
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  const video = document.createElement('video');
   video.autoplay = true;
   video.muted = true;
   video.srcObject = stream;
   await new Promise((resolve) => video.addEventListener('canplay', resolve, { once: true }));
   const width = video.videoWidth;
   const height = video.videoHeight;
-  const inputCanvas = document.createElement('canvas');
   inputCanvas.width = width;
   inputCanvas.height = height;
-  const inputCtx = inputCanvas.getContext('2d');
 
-  const outputCanvas = document.createElement('canvas');
   outputCanvas.width = width;
   outputCanvas.height = height;
-  const outputCtx = outputCanvas.getContext('2d');
+  outputCtx.fillStyle = 'white';
+  outputCtx.fillRect(0, 0, width, height);
+  outputCtx.fillStyle = `rgb(${~~(256 * Math.random())},${~~(256 * Math.random())},${~~(256 * Math.random())})`;
 
-  const ps = Array(NB_PS).fill(null).map(() => ({
+  const ps = Array(nb).fill(null).map(() => ({
     x: ~~((0.4 + 0.2 * Math.random()) * width),
     y: ~~((0.4 + 0.2 * Math.random()) * height),
   }));
 
-  document.body.style.transform = 'scaleX(-1)';
-  video.style.position = 'fixed';
-  video.style.opacity = '0.1';
-  document.body.append(video);
-  outputCanvas.style.position = 'fixed';
-  document.body.append(outputCanvas);
-
-  const animate = () => {
+  animate = () => {
     inputCtx.drawImage(video, 0, 0);
     const id = inputCtx.getImageData(0, 0, width, height);
     for (const p of ps) {
       const idi = 4 * (width * p.y + p.x);
-      p.x += ~~(SPEED * (-0.5 + id.data[idi] / 256));
-      p.y += ~~(SPEED * (-0.5 + id.data[idi + 1] / 256));
+      p.x += ~~(speed * (-0.5 + id.data[idi] / 256));
+      p.y += ~~(speed * (-0.5 + id.data[idi + 1] / 256));
       outputCtx.fillRect(p.x, p.y, 1, 1);
     }
     requestAnimationFrame(animate);
   };
   animate();
-})();
+};
+
+play(1000, 8);
+setTimeout(() => play(1000, 8), 2000);
