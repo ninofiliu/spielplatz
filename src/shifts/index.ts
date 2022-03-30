216ee7e489c7c9a675f022d353437cf0c9e7c52d
Nino Filiu <nino.filiu@gmail.com>
2022-03-30 23:14:21 +0200

    rename

--- src/shifts/index.ts
+++ src/shifts/index.ts
@@ -7,50 +7,53 @@
   const filesTxt = await filesResp.text();
   const files = await filesTxt.split('\n').filter(Boolean);
 
-  const sSrc = files[~~(Math.random() * files.length)];
-  console.log(sSrc);
-  const simg = document.createElement('img');
-  simg.src = sSrc;
-  await new Promise((r) => simg.addEventListener('load', r, { once: true }));
+  const warpSrc = files[~~(Math.random() * files.length)];
+  console.log(warpSrc);
+  const warpImg = document.createElement('img');
+  warpImg.src = warpSrc;
+  await new Promise((r) => warpImg.addEventListener('load', r, { once: true }));
 
-  const oSrc = files[~~(Math.random() * files.length)];
-  console.log(oSrc);
-  const oimg = document.createElement('img');
-  oimg.src = oSrc;
-  await new Promise((r) => oimg.addEventListener('load', r, { once: true }));
+  const initSrc = files[~~(Math.random() * files.length)];
+  console.log(initSrc);
+  const initImg = document.createElement('img');
+  initImg.src = initSrc;
+  await new Promise((r) => initImg.addEventListener('load', r, { once: true }));
 
-  const { width, height } = oimg;
+  const size = {
+    x: initImg.width,
+    y: initImg.height,
+  };
   const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = size.x;
+  canvas.height = size.y;
   document.body.append(canvas);
 
   const ctx = canvas.getContext('2d');
-  ctx.drawImage(simg, 0, 0, simg.width, simg.height, 0, 0, width, height);
-  const sid = ctx.getImageData(0, 0, width, height);
-  for (let i = 0; i < width * height; i += 4) {
+  ctx.drawImage(warpImg, 0, 0, warpImg.width, warpImg.height, 0, 0, size.x, size.y);
+  const sid = ctx.getImageData(0, 0, size.x, size.y);
+  for (let i = 0; i < size.x * size.y; i += 4) {
     for (let d = 0; d < 3; d++) {
       sid.data[i + d] = (~~sid.data[i + d] / compr) * compr;
     }
   }
 
-  ctx.drawImage(oimg, 0, 0);
+  ctx.drawImage(initImg, 0, 0);
   const animate = () => {
-    const oid = ctx.getImageData(0, 0, width, height);
-    const nid = new ImageData(width, height);
-    for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        const i = 4 * (width * y + x);
+    const oid = ctx.getImageData(0, 0, size.x, size.y);
+    const nid = new ImageData(size.x, size.y);
+    for (let x = 0; x < size.x; x++) {
+      for (let y = 0; y < size.y; y++) {
+        const i = 4 * (size.x * y + x);
         const dx = size * (-1 + 2 * sid.data[i + 0] / 256);
         const dy = size * (-1 + 2 * sid.data[i + 1] / 256);
         for (const fx of [0, 1]) {
           for (const fy of [0, 1]) {
-            const sx = (x + ~~dx + fx + width) % width;
-            const sy = (y + ~~dy + fy + height) % height;
-            const si = 4 * (width * sy + sx);
-            let px = (dx + width) % 1;
+            const sx = (x + ~~dx + fx + size.x) % size.x;
+            const sy = (y + ~~dy + fy + size.y) % size.y;
+            const si = 4 * (size.x * sy + sx);
+            let px = (dx + size.x) % 1;
             if (fx) px = 1 - px;
-            let py = (dy + height) % 1;
+            let py = (dy + size.y) % 1;
             if (fy) py = 1 - py;
             for (let d = 0; d < 4; d++) {
               nid.data[i + d] += oid.data[si + d] * px * py;
