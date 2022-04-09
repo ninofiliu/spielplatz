155b88e689e4f2ace56ff0fff65eb950b57064de
Nino Filiu <nino.filiu@gmail.com>
2022-04-10 00:51:07 +0200

    optimize

--- src/trees/index.ts
+++ src/trees/index.ts
@@ -14,9 +14,9 @@ type Leaf = {
   children: Leaf[];
 };
 
-const tortion = 0.2;
-const splitProb = 0.03;
-const splitAngle = 0.5;
+const tortion = 0.3;
+const splitProb = 0.04;
+const splitAngle = 0.1;
 
 const tree: Leaf[] = [
   {
@@ -25,6 +25,7 @@ const tree: Leaf[] = [
     children: [],
   },
 ];
+const leaves = new Set([tree[0]]);
 
 const dims = { x: window.innerWidth, y: window.innerHeight };
 const canvas = document.createElement("canvas");
@@ -36,7 +37,7 @@ document.body.append(canvas);
 const ctx = canvas.getContext("2d");
 
 const canvasPos = (pos: Cart) =>
-  [0.5 * dims.x + 10 * pos.x, dims.y - 10 * pos.y] as const;
+  [0.5 * dims.x + 5 * pos.x, dims.y - 5 * pos.y] as const;
 
 const addLeaves = (branch: Leaf, newLeaves: Leaf[]) => {
   for (const newLeaf of newLeaves) {
@@ -55,50 +56,60 @@ const addLeaves = (branch: Leaf, newLeaves: Leaf[]) => {
   ctx.strokeStyle = "white";
 
   const animate = () => {
-    tree.forEach((leaf) => {
-      if (leaf.children.length) return;
-      if (leaf.vel.r < 0.1) return;
-      if (Math.random() < splitProb) {
-        const leftVelT = leaf.vel.t - splitAngle;
-        const rightVelT = leaf.vel.t + splitAngle;
-        const leftLeaf: Leaf = {
-          pos: {
-            x: leaf.pos.x + leaf.vel.r * Math.cos(leftVelT),
-            y: leaf.pos.y + leaf.vel.r * Math.sin(leftVelT),
-          },
-          vel: {
-            r: leaf.vel.r * 0.8,
-            t: leftVelT,
-          },
-          children: [],
-        };
-        const rightLeaf: Leaf = {
-          pos: {
-            x: leaf.pos.x + leaf.vel.r * Math.cos(rightVelT),
-            y: leaf.pos.y + leaf.vel.r * Math.sin(rightVelT),
-          },
-          vel: {
-            r: leaf.vel.r * 0.8,
-            t: rightVelT,
-          },
-          children: [],
-        };
-        addLeaves(leaf, [leftLeaf, rightLeaf]);
-      } else {
-        const newLeaf: Leaf = {
-          pos: {
-            x: leaf.pos.x + leaf.vel.r * Math.cos(leaf.vel.t),
-            y: leaf.pos.y + leaf.vel.r * Math.sin(leaf.vel.t),
-          },
-          vel: {
-            r: leaf.vel.r,
-            t: leaf.vel.t + tortion * (-0.5 + Math.random()),
-          },
-          children: [],
-        };
-        addLeaves(leaf, [newLeaf]);
+    console.log(leaves.size);
+    if (leaves.size === 0) return;
+    const toAdd = new Set<Leaf>();
+    for (const leaf of leaves) {
+      if (leaf.vel.r > 0.2) {
+        if (Math.random() < splitProb) {
+          const leftVelT = leaf.vel.t - splitAngle;
+          const rightVelT = leaf.vel.t + splitAngle;
+          const leftLeaf: Leaf = {
+            pos: {
+              x: leaf.pos.x + leaf.vel.r * Math.cos(leftVelT),
+              y: leaf.pos.y + leaf.vel.r * Math.sin(leftVelT),
+            },
+            vel: {
+              r: leaf.vel.r * 0.8,
+              t: leftVelT,
+            },
+            children: [],
+          };
+          const rightLeaf: Leaf = {
+            pos: {
+              x: leaf.pos.x + leaf.vel.r * Math.cos(rightVelT),
+              y: leaf.pos.y + leaf.vel.r * Math.sin(rightVelT),
+            },
+            vel: {
+              r: leaf.vel.r * 0.8,
+              t: rightVelT,
+            },
+            children: [],
+          };
+          addLeaves(leaf, [leftLeaf, rightLeaf]);
+          toAdd.add(leftLeaf);
+          toAdd.add(rightLeaf);
+        } else {
+          const newLeaf: Leaf = {
+            pos: {
+              x: leaf.pos.x + leaf.vel.r * Math.cos(leaf.vel.t),
+              y: leaf.pos.y + leaf.vel.r * Math.sin(leaf.vel.t),
+            },
+            vel: {
+              r: leaf.vel.r,
+              t: leaf.vel.t + tortion * (-0.5 + Math.random()),
+            },
+            children: [],
+          };
+          addLeaves(leaf, [newLeaf]);
+          toAdd.add(newLeaf);
+        }
       }
-    });
+      leaves.delete(leaf);
+    }
+    for (const leaf of toAdd) {
+      leaves.add(leaf);
+    }
 
     requestAnimationFrame(animate);
   };
