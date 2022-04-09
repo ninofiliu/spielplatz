838831b960843f2f65ff5ddc3699e2a89b4cbdb8
Nino Filiu <nino.filiu@gmail.com>
2022-04-09 18:04:29 +0200

    fix pixels

--- src/shifts/pixels.ts
+++ src/shifts/pixels.ts
@@ -1,22 +1,22 @@
 import init from './init';
 
 (async () => {
-  const size = 2;
-  const compr = 128;
+  const size = 5;
+  const compr = 1;
   const shouldRecord = false;
   const dims = {
     x: 1080,
     y: 1080,
   };
 
-  const { ctx, initId } = await init(dims, compr, shouldRecord);
+  const { ctx, initId, warpId } = await init(dims, compr, shouldRecord);
 
   ctx.putImageData(initId, 0, 0);
 
   const t0 = performance.now();
   let step = 0;
 
-  const animate = () => {
+  const animate = async () => {
     step++;
     const t1 = performance.now();
     console.log(`${~~(1_000_000 * (t1 - t0) / (step * dims.x * dims.y))}ms/f/mp`);
@@ -26,8 +26,8 @@ import init from './init';
     for (let x = 0; x < dims.x; x++) {
       for (let y = 0; y < dims.y; y++) {
         const i = 4 * (dims.x * y + x);
-        const dx = -size + ~~(initId.data[i + 0] / 256 * (2 * size + 1));
-        const dy = -size + ~~(initId.data[i + 1] / 256 * (2 * size + 1));
+        const dx = -size + ~~(warpId.data[i + 0] / 256 * (2 * size + 1));
+        const dy = -size + ~~(warpId.data[i + 1] / 256 * (2 * size + 1));
         const sx = (x + dx + dims.x) % dims.x;
         const sy = (y + dy + dims.y) % dims.y;
         const si = 4 * (dims.x * sy + sx);
