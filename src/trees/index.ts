59dd6e55a8116220b456392f8cabb14c2cca1506
Nino Filiu <nino.filiu@gmail.com>
2022-04-10 00:22:22 +0200

    add branching

--- src/trees/index.ts
+++ src/trees/index.ts
@@ -14,7 +14,9 @@ type Leaf = {
   children: Leaf[];
 };
 
-const tortion = 2;
+const tortion = 0.2;
+const splitProb = 0.03;
+const splitAngle = 0.5;
 
 const tree: Leaf[] = [
   {
@@ -34,7 +36,7 @@ document.body.append(canvas);
 const ctx = canvas.getContext("2d");
 
 const canvasPos = (pos: Cart) =>
-  [0.5 * dims.x + 10 * pos.x, 0.5 * dims.y - 10 * pos.y] as const;
+  [0.5 * dims.x + 10 * pos.x, dims.y - 10 * pos.y] as const;
 
 const addLeaves = (branch: Leaf, newLeaves: Leaf[]) => {
   for (const newLeaf of newLeaves) {
@@ -49,13 +51,39 @@ const addLeaves = (branch: Leaf, newLeaves: Leaf[]) => {
 {
   ctx.lineWidth = 1;
   ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, dims[0], dims[1]);
+  ctx.fillRect(0, 0, dims.x, dims.y);
   ctx.strokeStyle = "white";
 
   const animate = () => {
     tree.forEach((leaf) => {
       if (leaf.children.length) return;
-      if (Math.random() < 0.01) {
+      if (leaf.vel.r < 0.1) return;
+      if (Math.random() < splitProb) {
+        const leftVelT = leaf.vel.t - splitAngle;
+        const rightVelT = leaf.vel.t + splitAngle;
+        const leftLeaf: Leaf = {
+          pos: {
+            x: leaf.pos.x + leaf.vel.r * Math.cos(leftVelT),
+            y: leaf.pos.y + leaf.vel.r * Math.sin(leftVelT),
+          },
+          vel: {
+            r: leaf.vel.r * 0.8,
+            t: leftVelT,
+          },
+          children: [],
+        };
+        const rightLeaf: Leaf = {
+          pos: {
+            x: leaf.pos.x + leaf.vel.r * Math.cos(rightVelT),
+            y: leaf.pos.y + leaf.vel.r * Math.sin(rightVelT),
+          },
+          vel: {
+            r: leaf.vel.r * 0.8,
+            t: rightVelT,
+          },
+          children: [],
+        };
+        addLeaves(leaf, [leftLeaf, rightLeaf]);
       } else {
         const newLeaf: Leaf = {
           pos: {
